e398167be93bca324273ce7d27cac6435759d216
Wilson (admin precon) <administrador_it@grupoprecon.com>
2026-08-03 16:05:28 -0600

    chore(config): actualizar fuentes CSS globales y script de desarrollo turbo

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
   cacheComponents: true,
   reactCompiler: true,
   experimental: {
-    optimizePackageImports: ["lucide-react"],
+    optimizePackageImports: ["lucide-react", "@hugeicons/react"],
   },
 };
 
